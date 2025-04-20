19bc7fb27ab15e5c860b8f54444b4ccab4225015
nuwanmataraarachchi <nuwancma@gmail.com>
2025-04-20 21:42:32 +0530

    Initial Commit

---
+++ EdgeLab/EdgeLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeLabApp.swift
+//  EdgeLab
+//
+//  Created by Nuwan Mataraarachchi on 2025-04-20.
+//
+
+import SwiftUI
+
+@main
+struct EdgeLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EdgeLabTests/EdgeLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeLabTests.swift
+//  EdgeLabTests
+//
+//  Created by Nuwan Mataraarachchi on 2025-04-20.
+//
+
+import Testing
+@testable import EdgeLab
+
+struct EdgeLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
